5037a3364bc22a394738488727ef5def66153ea5
Justin Ravat <justin.ravat@protonmail.com>
2022-12-05 13:51:10 +0100

    bind id's

--- controllers/movie.js
+++ controllers/movie.js
@@ -24,6 +24,8 @@ exports.getOneMovie = (req, res, next) => {
         return res.status(404).send(new Error('Movie not found!'));
       }
       movie.imageUrl = req.protocol + '://' + req.get('host') + '/images/' + movie.imageUrl;
+    //   pour afficher une image (contenue dans bgUrl par exemple) depuis le backend (attention, juste sur la fiche de détail dans ce cas précis)
+      movie.bgUrl = req.protocol + '://' + req.get('host') + '/images/' + movie.bgUrl;
       res.status(200).json(movie);
     }
   ).catch(
--- models/Movie.js
+++ models/Movie.js
@@ -1,5 +1,6 @@
 const movies = [
     {
+      "_id" : "d192ed99-b7ae-4689-82fa-d41e9b957fc5", 
       "titre" : "13: Game of Death",
       "realisateur" : "Chookiat Sakveerakul",
       "description" : "After losing his job, his car and his money, Phuchit, Krissada Sukosol, races against time to complete 13 tasks ordered by an anonymous caller who promised 100,000,000 Thai Baht upon completion.",
@@ -7,6 +8,7 @@ const movies = [
       "imageUrl" : "13_game_of_death.jpg"
     },
     {
+        "_id" : "915d3693-f57c-4770-981a-b71990bdd707",
       "titre" : "Alien",
       "realisateur" : "Ridley Scott",
       "description" : "The crew of a commercial spacecraft encounter a deadly lifeform after investigating an unknown transmission.",
@@ -14,6 +16,7 @@ const movies = [
       "imageUrl" : "alien.jpg"
     },
     {
+        "_id" : "336d87a7-4765-449e-ac18-116412ef9e43",
       "titre" : "Magnolia",
       "realisateur" : "Paul Thomas Anderson",
       "description" : "An epic mosaic of interrelated characters in search of love, forgiveness and meaning in the San Fernando Valley.",
@@ -21,6 +24,7 @@ const movies = [
       "imageUrl" : "magnolia.jpg"
     },
     {
+        "_id" : "b05eb63b-8e5e-4b6e-a600-c19f2cbb44f7",
       "titre" : "Donnie Darko",
       "realisateur" : "Richard Kelly",
       "description" : "After narrowly escaping a bizarre accident, a troubled teenager is plagued by visions of a man in a large rabbit suit who manipulates him to commit a series of crimes.",
@@ -28,6 +32,7 @@ const movies = [
       "imageUrl" : "donnie_darko.jpg"
     },
     {
+        "_id" : "b637be35-f49e-46ed-9d7c-20c84504f37d",
       "titre" : "Dobermann",
       "realisateur" : "Jan Kounen",
       "description" : "Dobermann is the world's most ruthless bank robber and with his gang rob bank after bank, now in Paris. What can the police do but to let the mad, morally bankrupt police commissioner loose on him?",
@@ -35,11 +40,13 @@ const movies = [
       "imageUrl" : "dobermann.jpg"
     },
     {
+        "_id" : "dddcbf1d-7485-46f1-94ca-2da89794d7e8",
       "titre" : "Starship Troopers",
       "realisateur" : "Paul Verhoeven",
       "description" : "Humans in a fascist, militaristic future wage war with giant alien bugs.",
       "dureeTotale" : 129,
-      "imageUrl" : "starship_troopers.jpg"
+      "imageUrl" : "starship_troopers.jpg",
+    //   "bgUrl" : "starship_troopers.jpg"
     }
 ];
 
