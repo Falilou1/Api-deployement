723d4d873c01fde2ae91d5d62f945f2e1e7f95ec
falou40@gmail.com <falou40@gmail.com>
2022-12-05 15:21:05 +0100

    my change

--- models/Movie.js
+++ models/Movie.js
@@ -5,7 +5,10 @@ const movies = [
     "realisateur": "Michael Hirst",
     "description": "Scandinavie, à la fin du 8ème siècle. Ragnar Lodbrok, un jeune guerrier viking, est avide d'aventures et de nouvelles conquêtes. Lassé des pillages sur les terres de l'Est, il se met en tête d'explorer l'Ouest par la mer.",
     "dureeTotale": 120,
-    "imageUrl": "viking.jpg"
+    "imageUrl": "viking.jpg",
+    "_id": "Viking",
+    "bandeAnnonce": "https://www.youtube.com/embed/DG_dPkDtAI0"
+
     
   },
 
@@ -14,35 +17,45 @@ const movies = [
       "realisateur": "David Benioff",
       "description": "Neuf familles nobles rivalisent pour le contrôle du Trône de Fer dans les sept royaumes de Westeros. Pendant ce temps, des anciennes créatures mythiques oubliées reviennent pour faire des ravages.",
       "dureeTotale": 117,
-      "imageUrl": "game-of-thrones.jpg"
+      "imageUrl": "game-of-thrones.jpg",
+      "_id": "Gameofthrones",
+      "bandeAnnonce": "https://www.youtube.com/embed/aAF12LNAeNI"
     },
     {
       "titre": "Power",
       "realisateur": "50cent",
       "description": "James St. Patrick, surnommé Ghost, est le propriétaire d'une boîte de nuit populaire à New York. En parallèle, il est un acteur majeur dans l'un des plus grands réseaux de drogues illicites de la ville.",
       "dureeTotale": 40,
-      "imageUrl": "power.jpg"
+      "imageUrl": "power.jpg",
+      "_id": "Power",
+      "bandeAnnonce": "https://www.youtube.com/embed/94Z7yqmlNq0"
     },
     {
       "titre": "Ozark",
       "realisateur": "Bill Dubuque",
       "description": "Marty blanchit de l'argent pour le compte d'un baron de la drogue. Lorsque son partenaire le trahit, il déménage avec sa famille aux monts Ozarks. Là-bas, il se retrouve opposé à un dealer local, à une bande de voyous et à un agent du FBI tenace.",
       "dureeTotale": 700,
-      "imageUrl": "ozark.jpg"
+      "imageUrl": "ozark.jpg",
+      "_id": "Ozarks",
+      "bandeAnnonce": "https://www.youtube.com/embed/pJlLLif3v9k"
     },
     {
       "titre": "Blacklist",
       "realisateur": "Jon Bokenkamp",
       "description": "L'ex-agent du gouvernement Raymond Red Reddington est l'un des fugitifs les plus recherchés au monde. Il accepte de collaborer avec le FBI à condition de travailler avec la profileuse Elizabeth Keen.",
       "dureeTotale": 900,
-      "imageUrl": "blacklist.jpg"
+      "imageUrl": "blacklist.jpg",
+      "_id": "Blacklist",
+      "bandeAnnonce": "https://www.youtube.com/embed/DG_dPkDtAI0"
     },
     {
       "titre": "Narcos",
       "realisateur": "Chris Brancato",
       "description": "Inspirée de l'histoire du célèbre narcotrafiquant de Medellín Pablo Escobar, cette série raconte la guerre sanglante des cartels en Colombie.",
       "dureeTotale": 70,
-      "imageUrl": "narcos.jpg"
+      "imageUrl": "narcos.jpg",
+      "_id": "narcos",
+      "bandeAnnonce": "https://www.youtube.com/embed/sFlUFVJFGNA"
     }
 ];
 
